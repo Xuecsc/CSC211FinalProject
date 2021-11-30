8311386ec7b44b0176b5ffd050d60d8f01ed0386
joleane li <95319781+Xuecsc@users.noreply.github.com>
2021-11-30 23:38:33 +0000

    add Point.h and Point.cpp file, and run main.cpp

---
+++ Point.h
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class Point{
+private:
+  int x;
+  int y;
+
+public:
+  Point();
+  Point(int,int);
+  int getX();
+  int getY();
+  void print();
+
+};
--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
+#include "Point.h"
 
 int main()
 {
   cout << "Hello World !" << endl;
+  Point point;
+
+  Point pointP(10,15);
+
+  point.print();
+  pointP.print();
+  
 }
+
